214334bdb216672f26788a31585ddcdf39ef15bf
SunshineBean-N <nikolasbernabeu@gmail.com>
2026-02-23 22:08:34 +0100

    V3
    Resoluciones ajustadas
    
    Escena Menu
    -Input Fied 8 caracteres
    
    Escena Video
    -Botones hechos
    
    Escena Jugar
    -Botones hechos
    
    Escena Opciones
    -Juego: Dificultad
    -Video: Brollo, Contraste, Resolucion
    -Audio: Mute, Volumen, Musica, Voces, Dialogo

--- Assets/Script/Paneles.cs
+++ Assets/Script/Paneles.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class Paneles : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject panel;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void TogglePanel() {  panel.SetActive(!panel.activeSelf); }
+
+    public void OpenPanel() { panel.SetActive(true); }
+    public void ClosePanel() { panel.SetActive(false); }
 }
