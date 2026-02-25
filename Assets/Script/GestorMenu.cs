aea8adb3129c9ff7ce166368e5a00458c5dc0d93
SunshineBean-N <nikolasbernabeu@gmail.com>
2026-02-25 11:34:01 +0100

    V3.1
    Necesita actualizar paneles de menu a nuevo metodo
    Implementar animaciones y audio
    Ajustar nuevos botones a cambio 1:1 con panel

--- Assets/Script/GestorMenu.cs
+++ Assets/Script/GestorMenu.cs
@@ -5,8 +5,56 @@ public class JestorMEnu : MonoBehaviour
     public bool controlPanel;
     public string nombreAnimacion;
     public Animator animator;
+    //No se usa, primer codigo de animacion
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Animator LogrosEntrada;
+    public Animator CreditosEntrada;
+    public Animator RecordsEntrada;
+    public Animator TutorialEntrada;
+
+    public int AnimacionSwitch;
+    //Apunta al "numero" de el menu
+
+    public void EntraMenus()
+    {
+        switch (AnimacionSwitch)
+        { case 1:
+                LogrosEntrada.Play("PanelLogrosEntrada");
+                break;
+        case 2:
+                CreditosEntrada.Play("PanelMuestra");
+                break;
+        }
+    }
+
+    public void SalidaMenus()
+    {
+        switch (AnimacionSwitch)
+        {
+            case 1:
+                LogrosEntrada.Play("PanelLogrosSalida");
+                break;
+            case 2:
+                CreditosEntrada.Play("PanelMuestraSalida");
+                break;
+        }
+    }
+
+    //llamar con el onclick, asignar AnimationSwitch el valor uno e indicar que estoy en el metodo "EntraLogros"
+    public void LogrosAnimacion()
+    {
+        SalidaMenus();
+        AnimacionSwitch = 1;
+        EntraMenus();
+    }
+    public void CreditosAnimacion()
+    {
+        SalidaMenus();
+        AnimacionSwitch = 2;
+        EntraMenus();
+    }
+
     public void ControlAnimacion()
     {
         if (controlPanel) { nombreAnimacion = "nombrequecorresponda"; }
@@ -14,10 +62,6 @@ public class JestorMEnu : MonoBehaviour
         animator.Play(nombreAnimacion);
         controlPanel = !controlPanel;
     }
+    //No se usa, primer codigo de animacion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Script/ValoresInput.cs
+++ Assets/Script/ValoresInput.cs
@@ -23,4 +23,20 @@ public class ValoresInput : MonoBehaviour
     {    string texto = IdentificadorTexto.text;
      IdentificadorTexto.text = texto; }
     // el primero identifica el marco, el segundo el texto que tu vas a escribir, (recuerda en el inspector en + añadir LeerField que es el nombre) entender porque no se escribe string en el parentesis **
+
+    [Header("Slider")]
+    public Slider miSlider;
+    public TMP_Text textoValorSlider;
+
+    public void LeerValorSlider()
+    { float valor = miSlider.value;
+    textoValorSlider.text = valor.ToString();}
+
+    [Header("Dropdown")]
+    public TMP_Dropdown miDropdown;
+    public TMP_Text textoValorDropdown;
+
+    public void LeerDropdown()
+    { float valor = miDropdown.value;
+        textoValorDropdown.text = valor.ToString();}
 }
